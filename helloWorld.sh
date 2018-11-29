7220a35688d24e676f8795fc69106769a6a271e7
william <luroylau@163.com>
2018-11-29 22:27:44 +0800

    Hello world for shell

---
+++ helloWorld.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+echo "Hello, world!"
+
+# 执行脚本的方法
+# ---------------------
+# -> 可执行文件
+# chmod +x helloWorld.sh 使脚本可执行
+# ./helloWorld.sh 来执行脚本
+# ---------------------
+# -> 解释器参数
+# /bin/zsh helloWorld.sh
